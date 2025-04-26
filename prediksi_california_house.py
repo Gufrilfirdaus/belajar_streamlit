3b3a5170663aecaa4c0615e2a2f3d5e6f1719ef2
Muhammad Gufril Firdaus <gufril57@gmail.com>
2025-04-26 19:53:00 +0700

    Update prediksi_california_house.py

--- prediksi_california_house.py
+++ prediksi_california_house.py
@@ -1,64 +1,82 @@
-# prediksi_california_house.py
-
 import streamlit as st
 import pandas as pd
-import shap
+import base64
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_california_housing
-from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
+import numpy as np
+import yfinance as yf
 
-def prediksi_california_house():
-    housing = fetch_california_housing(as_frame=True)
-    X = housing.data
-    Y = housing.target
+st.title('S&P 500 App')
 
-    st.write("""
-    # California House Price Prediction App 🏡
-    
-    This app predicts the **California House Price**!
-    """)
-    st.write('---')
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
 
-    # Sidebar Input
-    st.sidebar.header('Specify Input Parameters')
+st.sidebar.header('User Input Features')
 
-    def user_input_features():
-        data = {}
-        for col in X.columns:
-            data[col] = st.sidebar.slider(col, float(X[col].min()), float(X[col].max()), float(X[col].mean()))
-        features = pd.DataFrame(data, index=[0])
-        return features
+# Web scraping of S&P 500 data
+#
+@st.cache
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    return df
 
-    df = user_input_features()
+df = load_data()
+sector = df.groupby('GICS Sector')
 
-    # Show input
-    st.header('Specified Input Parameters')
-    st.write(df)
-    st.write('---')
+# Sidebar - Sector selection
+sorted_sector_unique = sorted( df['GICS Sector'].unique() )
+selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
 
-    # Train model
-    model = RandomForestRegressor()
-    model.fit(X, Y)
+# Filtering data
+df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
 
-    # Prediction
-    prediction = model.predict(df)
+st.header('Display Companies in Selected Sector')
+st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector)
 
-    st.header('Prediction of Median House Value')
-    st.write(f"${prediction[0] * 100_000:,.2f}")
-    st.write('---')
+# Download S&P500 data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
 
-    # SHAP Explanation
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(X)
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
 
-    st.header('Feature Importance')
+# https://pypi.org/project/yfinance/
 
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    plt.title('Feature Importance based on SHAP Values')
-    shap.summary_plot(shap_values, X)
-    st.pyplot(bbox_inches='tight')
-    st.write('---')
+data = yf.download(
+        tickers = list(df_selected_sector[:10].Symbol),
+        period = "ytd",
+        interval = "1d",
+        group_by = 'ticker',
+        auto_adjust = True,
+        prepost = True,
+        threads = True,
+        proxy = None
+    )
 
-    plt.title('Feature Importance (Bar)')
-    shap.summary_plot(shap_values, X, plot_type="bar")
-    st.pyplot(bbox_inches='tight')
+# Plot Closing Price of Query Symbol
+def price_plot(symbol):
+  df = pd.DataFrame(data[symbol].Close)
+  df['Date'] = df.index
+  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+  plt.xticks(rotation=90)
+  plt.title(symbol, fontweight='bold')
+  plt.xlabel('Date', fontweight='bold')
+  plt.ylabel('Closing Price', fontweight='bold')
+  return st.pyplot()
+
+num_company = st.sidebar.slider('Number of Companies', 1, 5)
+
+if st.button('Show Plots'):
+    st.header('Stock Closing Price')
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
